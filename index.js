e53a3495571bda5c74dc3cdd103be06d89783718
tony <effrenanthony2704@gmail.com>
2021-03-06 20:20:32 -0500

    with cors

--- index.js
+++ index.js
@@ -8,20 +8,22 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 var allowedOrigins = ['http://localhost:3000',
                       'https://effrenanthony.vercel.app'];
+// app.use(cors({
+//   origin: function(origin, callback){
+//     // allow requests with no origin 
+//     // (like mobile apps or curl requests)
+//     if(!origin) return callback(null, true);
+//     if(allowedOrigins.indexOf(origin) === -1){
+//       var msg = 'The CORS policy for this site does not ' +
+//                 'allow access from the specified Origin.';
+//       return callback(new Error(msg), false);
+//     }
+//     return callback(null, true);
+//   }
+// }));
 app.use(cors({
-  origin: function(origin, callback){
-    // allow requests with no origin 
-    // (like mobile apps or curl requests)
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){
-      var msg = 'The CORS policy for this site does not ' +
-                'allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  }
+  origin: 'http://localhost:3000'
 }));
-
 app.use(require('./app/routes/contact.js'))
 
 const port = process.env.PORT || 3001;
